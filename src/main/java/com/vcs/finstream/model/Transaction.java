b02f491664f52d2091ef46805ec53ff61e685c84
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2026-02-21 14:51:15 +0530

    modified: Transaction to use Lombok annotations

--- src/main/java/com/vcs/finstream/model/Transaction.java
+++ src/main/java/com/vcs/finstream/model/Transaction.java
@@ -1,8 +1,13 @@
 package com.vcs.finstream.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@Getter
 public class Transaction {
 
     private final String transactionId;
@@ -11,27 +16,5 @@ public class Transaction {
     private final TransactionType type;
     private final Category category;
     private final LocalDateTime timestamp;
-
-    public Transaction(
-            String transactionId,
-            String userId,
-            BigDecimal amount,
-            TransactionType type,
-            Category category,
-            LocalDateTime timestamp) {
-        this.transactionId = transactionId;
-        this.userId = userId;
-        this.amount = amount;
-        this.type = type;
-        this.category = category;
-        this.timestamp = timestamp;
-    }
-
-    public String getTransactionId() {return transactionId;}
-    public String getUserId() {return userId;}
-    public BigDecimal getAmount() {return amount;}
-    public TransactionType getType() {return type;}
-    public Category getCategory() {return category;}
-    public LocalDateTime getTimestamp() {return timestamp;}
 
 }
